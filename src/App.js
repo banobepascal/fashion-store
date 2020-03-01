55c3dec03bb5a8433076f0faeff77515539de5e9
banobepascal <banobepascal@gmail.com>
2020-03-01 15:35:39 +0300

    ft(update app with Shop-Page):
    - created shop-page
    - created collection preview component
    - created collection item component
    - added styling to App css
    - updated App.js
    
    Finishes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Route } from "react-router-dom";
 import "./App.css";
 
 import Homepage from "./pages/homepage/Homepage.component";
+import ShopPage from "./pages/shop/Shop.component";
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={Homepage}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </div>
   );
 }
