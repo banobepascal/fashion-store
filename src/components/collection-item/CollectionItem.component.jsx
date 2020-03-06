e8f13f301ca0fa75eabd1a40d7be84e3718fb785
banobepascal <banobepascal@gmail.com>
2020-03-06 22:26:35 +0300

    ch(implenting redux in Cart):
    - styled cart-item and custom-button in cartCollection component
    - added cart-item reducer to cart-collection
    
    Finishes

--- src/components/collection-item/CollectionItem.component.jsx
+++ src/components/collection-item/CollectionItem.component.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./CollectionItem.styles.scss";
+import CustomButton from "../Custom-Button/Custom-Button.component";
+import { addItem } from "../../redux/Cart/cart.actions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
-    <div className="collection-item">
-      <div style={{ backgroundImage: `url(${imageUrl}` }} className="image" />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
+    <div className='collection-item'>
+      <div style={{ backgroundImage: `url(${imageUrl}` }} className='image' />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
