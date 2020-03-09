65f403cfdadc59d3012236edb30ace766b775f11
banobepascal <banobepascal@gmail.com>
2020-03-09 19:53:06 +0300

    bg(fix on signin respomsiveness)
    Finishes

--- src/components/collection-preview/CollectionPreview.component.jsx
+++ src/components/collection-preview/CollectionPreview.component.jsx
@@ -6,7 +6,7 @@ import CollectionItem from '../collection-item/CollectionItem.component';
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <h1 className="title">{title}</h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
