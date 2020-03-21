40eb64580781f1402ab2678b187f470fac0552fa
banobepascal <banobepascal@gmail.com>
2020-03-21 19:39:11 +0300

    ch(implented responsiveness for sign-up and sign-in components): and sign-in-sign-up page

--- src/components/Sign-In/Sign-in.styles.js
+++ src/components/Sign-In/Sign-in.styles.js
@@ -4,6 +4,10 @@ export const SignInContainer = styled.div`
   width: 380px;
   display: flex;
   flex-direction: column;
+
+  @media screen and (max-width: 400px) {
+    width: 80vw;
+  }
 `;
 
 export const SignInTitle = styled.h2`
@@ -13,4 +17,10 @@ export const SignInTitle = styled.h2`
 export const ButtonsBarContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media screen and (max-width: 400px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+  }
 `;
